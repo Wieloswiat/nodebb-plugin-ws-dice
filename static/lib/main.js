228e81d9a3e2bc822f067e447e0067ef31cdee8c
oplik0 <opliko.reg@protonmail.com>
2022-07-30 13:28:22 +0200

    fix: eslint config

--- static/lib/main.js
+++ static/lib/main.js
@@ -2,7 +2,7 @@
 
 /* globals document, $ */
 
-$(document).ready(function () {
+$(document).ready(() => {
 	/*
 		This file shows how client-side javascript can be included via a plugin.
 		If you check `plugin.json`, you'll see that this file is listed under "scripts".
@@ -11,8 +11,8 @@ $(document).ready(function () {
 
 		Some events you can elect to listen for:
 
-		$(document).ready();			Fired when the DOM is ready
-		$(window).on('action:ajaxify.end', function(data) { ... });			"data" contains "url"
+		$(document).ready(); Fired when the DOM is ready
+		$(window).on('action:ajaxify.end', function(data) { ... }); "data" contains "url"
 	*/
 	// Note how this is shown in the console on the first load of every page
 	socket.on('event:new_post', (data) => {
