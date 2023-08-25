35afe6d77ea25b1fb9df8903f1c97ceda0241b39
Opliko <opliko.reg@protonmail.com>
2023-08-25 23:23:34 +0200

    fix: lint issues

--- test/index.js
+++ test/index.js
@@ -17,16 +17,14 @@
 'use strict';
 
 /* globals describe, it, before */
+/* eslint-disable no-unused-vars */
 
 const assert = require('assert');
 
-const winston = require.main.require('winston');
 
 const topics = require.main.require('./src/topics');
-const { search } = require.main.require('./src/search');
 const categories = require.main.require('./src/categories');
 const user = require.main.require('./src/user');
-const plugins = require.main.require('./src/plugins');
 
 
 describe('nodebb-plugin-ws-dice', () => {
@@ -61,14 +59,14 @@ describe('nodebb-plugin-ws-dice', () => {
 
 	it('should roll the dice for a simple case', async () => {
 		await topics.reply({
-            uid: commenterUid,
-            tid: topicData.tid,
-            content: '/roll 1d6',
-        })
+			uid: commenterUid,
+			tid: topicData.tid,
+			content: '/roll 1d6',
+		});
 		const events = await topics.events.get(topicData.tid, commenterUid);
-        assert.strictEqual(events.length, 1);
-        assert.strictEqual(events[0].type, 'dice');
-        assert(events[0].total <= 6);
-        assert(events[0].total >= 1);
+		assert.strictEqual(events.length, 1);
+		assert.strictEqual(events[0].type, 'dice');
+		assert(events[0].total <= 6);
+		assert(events[0].total >= 1);
 	});
 });
