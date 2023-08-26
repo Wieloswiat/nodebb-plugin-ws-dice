f7b0d067bec380614c526c01ec6445d8541a7913
Opliko <opliko.reg@protonmail.com>
2023-08-26 03:26:14 +0200

    fix: pass in an array

--- static/lib/main.js
+++ static/lib/main.js
@@ -7,7 +7,7 @@ define('plugins/dice/main', [
 	socket.on('plugins:dice:event:new_roll', (data) => {
 		if (Array.isArray(data) && data.length && data.some(e => e.type === 'dice')) {
 			for (const event of data) {
-				hooks.one('action:posts.loaded', () => posts.addTopicEvents(event));
+				hooks.one('action:posts.loaded', () => posts.addTopicEvents([event]));
 			}
 		}
 	});
